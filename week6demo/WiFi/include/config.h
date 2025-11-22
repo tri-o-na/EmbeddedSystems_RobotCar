98f01f2fbf5f256da91f04a16abaa9324409efb3
kaiii299 <2401566@sit.singaporetech.edu.sg>
2025-11-22 21:10:48 +0800

    Remove wifi file

--- week6demo/WiFi/include/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Change these to your PHONE HOTSPOT credentials (2.4 GHz)
-#define WIFI_SSID      "GWiPhone"
-#define WIFI_PASSWORD  ""
-#define TCP_PORT      80   // use 80 to access from a browser at http://<Pico-IP>/
-#define MAX_ROWS      128          // how many rows to keep (ring buffer)
-#define MAX_TYPE_LEN  16
-#define MAX_MSG_LEN   128
